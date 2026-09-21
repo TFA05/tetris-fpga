d3cf835697d99dd9b26ff835ddca933ede6cc1c1
VukasinDvo <vukasin2005@gmail.com>
2026-09-21 21:02:30 +0200

    Test

--- TetrisProgram/main.c
+++ TetrisProgram/main.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "game.h"
-
+#include "gfx.h"
+/*
 int main(void)
 {
     gameInit();
@@ -9,4 +10,15 @@ int main(void)
         gameUpdate();
     }
     return 0;
+}
+*/
+
+int main(void)
+{
+    out(REG_SEG7, 0x123456);                                /* CPU živ? */
+    gfxFillRect(0, 0, SCREEN_W - 1, SCREEN_H - 1, 0xF00);   /* crveno */
+    gfxWaitVsync();
+    gfxSwapBuffers();
+    gfxFillRect(0, 0, SCREEN_W - 1, SCREEN_H - 1, 0xF00);
+    while (1) ;
 }
