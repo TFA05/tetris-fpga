6f5954e9ca31ba907a20f624bb10a10d3f24a533
jaki <95507372+ANDREJAKI@users.noreply.github.com>
2026-09-22 11:26:30 +0200

    Update io.c

--- TetrisProgram/io.c
+++ TetrisProgram/io.c
@@ -1,11 +1,11 @@
 #include "io.h"
-
+//Asemblerski kod za store
 __attribute__((naked)) void out(unsigned int address __attribute__((unused)), unsigned int value __attribute__((unused)))
 {
     asm("sw a1,0(a0)");
     asm("ret");
 }
-
+//Asemblerski kod za load
 __attribute__((naked)) unsigned int in(unsigned int address __attribute__((unused)))
 {
     asm("lw a0,0(a0)");
