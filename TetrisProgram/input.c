a77653c90c62f152a51b4db899f1817e57a1ad37
jaki <95507372+ANDREJAKI@users.noreply.github.com>
2026-09-22 11:34:03 +0200

    Update input.c

--- TetrisProgram/input.c
+++ TetrisProgram/input.c
@@ -6,6 +6,7 @@ static dword_t g_keyState = 0;
 static dword_t g_prevKeyState = 0;
 static dword_t g_nextRepeat[32];
 
+//Sacuva trenutno stanje g_keyState u g_prevKeyState i procita sveze stanje sa hardvera
 void inputRead(void)
 {
     g_prevKeyState = g_keyState;
@@ -35,13 +36,18 @@ bool_t inputRepeat(dword_t bit, dword_t delayMs, dword_t rateMs)
 {
     dword_t now = in(REG_TIMER);
 
+    //Taster je bas sad pritisnut prvi put
     if (inputPressed(bit)) {
+        //Zakazi prvo ponavljanje tek posle delayMs
         g_nextRepeat[bit] = now + delayMs;
         return TRUE;
     }
+    //Taster se i dalje drzi
     if (inputHeld(bit) && (sdword_t)(now - g_nextRepeat[bit]) >= 0) {
+        //Zakazi sledece ponavljanje posle rateMs
         g_nextRepeat[bit] = now + rateMs;
         return TRUE;
     }
+    //Ili se taster ne drzi uopste ili jos nije doslo vreme za sledece ponavljanje
     return FALSE;
 }
