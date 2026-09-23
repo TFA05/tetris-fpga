bf6f6636d209293f3520db6fd0b5459ecf9ec0c3
jaki <95507372+ANDREJAKI@users.noreply.github.com>
2026-09-23 10:10:52 +0200

    Update start.c

--- TetrisProgram/start.c
+++ TetrisProgram/start.c
@@ -5,8 +5,6 @@ extern int main(void);
 extern void _isr(void);
 void _start_c(void);
 
-/* Na adresi 0 pocinje program, a na adresi 0x10 je skok u prekidnu rutinu
-   (procesor tamo skace kada stigne zahtev za prekid). */
 __attribute__((naked, section(".text.start"))) void _start(void)
 {
     asm volatile(
